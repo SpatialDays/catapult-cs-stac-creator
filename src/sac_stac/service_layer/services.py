f4cb4628ce5dd815b3f986c54e3ab98b0eaa8953
Emily Selwood <emily.selwood@sa.catapult.org.uk>
2022-11-01 16:42:23 +0000

    polygon-conversion

--- src/sac_stac/service_layer/services.py
+++ src/sac_stac/service_layer/services.py
@@ -229,9 +229,11 @@ def add_stac_item(repo: S3Repository, acquisition_key: str, update_collection_on
 def create_geom(geometry, crs):
 
     if isinstance(geometry, Polygon):
-        poly = GeoSeries([Polygon(geometry.exterior)], crs=crs).to_crs(GENERIC_EPSG).to_json()
+        poly = GeoSeries([geometry.exterior], crs=crs).to_crs(GENERIC_EPSG).to_json()
         result = json.loads(poly)
-        return result.get('features')[0].get('geometry')
+        geom = result.get('features')[0].get('geometry')
+        geom['type'] = 'Polygon'
+        return geom
     else:
         poly = GeoSeries([geometry], crs=crs).to_crs(GENERIC_EPSG).to_json()
         result = json.loads(poly)
